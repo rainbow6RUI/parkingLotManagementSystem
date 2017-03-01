fdcd67c701ea21615b11881df5a0542f16e2da85
Messiforever <lyon724691463@163.com>
2017-03-01 14:10:30 +0800

    index页

--- scripts/homePage/directive.js
+++ scripts/homePage/directive.js
@@ -10,8 +10,8 @@
                     template:
                             "<div class='header'>"+
                                 "<img src='img/logo.png' alt='#'>"+
-                                "<div class='ion-laptop'><span>管理员登录</span></div>"+
-                                "<div class='ion-android-contacts'><span>工作人员交接</span></div>"+
+                                "<div class='ion-laptop' ui-sref='login' ui-sref-active=''><span>管理员登录</span></div>"+
+                                "<div class='ion-android-contacts' ui-sref='changejob' ui-sref-active=''><span>工作人员交接</span></div>"+
                                 "<div ><input type='text' placeholder='请输入查找所需关键字'/><div class='ion-android-search'></div></div>"+
                                 "<div class='ion-android-textsms'><span>站间联络</span></div>"+
                             "</div>"+
